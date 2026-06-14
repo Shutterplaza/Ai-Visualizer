6e27781458f7d4c80b6501498bf0a3c85c19f545
Shutterplaza <eddiejkb@hotmail.com>
2026-06-14 14:34:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
